723453572a4b7710e5e935e86c52d253bf97ab53
Nathanial Fabricatore <p23nfabricatore@fairfieldprep.org>
2022-10-20 23:31:30 -0400

    submit cycle 4 labs

---
+++ int(a).py
@@ -0,0 +1,22 @@
+int(a)
+Traceback (most recent call last):
+FIle "<pyshell#14>", line 1 <module>
+int(a)
+ValueError: invalid literal for int() with base 10: a
+int('a')
+Traceback (most recent call last):
+FIle "<pyshell#14>", line 1 <module>
+int(''a)
+ValueError: invalid literal for int() with base 10: 'a'
+'a'+2
+Traceback (most recent call last):
+File "<pyshell#23>", line 1, in <module>
+'a'+2
+TypeError: must be str, not int
+import date 
+pi+date.pi
+print(pi)
+SyntaxError: unexpected indent
+pring="im a happy camper
+print(pringg)
+Traceback (most recent call last):
